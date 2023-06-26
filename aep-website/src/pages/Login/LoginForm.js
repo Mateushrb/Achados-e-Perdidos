39f7faa64110ce2d1f1d31d86ad77a382a9a7ded
Carlos <henriqueeb@live.com>
2023-06-26 17:24:38 -0300

    feat(Loginform) adjust loginform

--- aep-website/src/pages/Login/LoginForm.js
+++ aep-website/src/pages/Login/LoginForm.js
@@ -1,17 +1,39 @@
 import React from 'react';
+import Input from './components/Input';
+import Button from './components/Button';
+import useForm from '../../Hooks/useForm';
 
-const LoginForm = ({ handleEmailChange, handlePasswordChange, handleSubmit }) => {
+const LoginForm = () => {
+  const username = useForm('email');
+  const password = useForm();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (username.validate() && password.validate()) {
+      fetch('http://45.235.53.125:8080/administradores', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }).then((response) => {
+        console.log(response)
+        return response.json();
+      }).then((json) => {
+        console.log(json)
+      })
+    }
+  }
+
   return (
-    <form className='container__login-form' onSubmit={handleSubmit}>
-      <h2>Admin Login</h2>
-      <label htmlFor="email">E-mail:</label>
-      <input type="email" id="email" onChange={handleEmailChange} />
-
-      <label htmlFor="password">Senha:</label>
-      <input type="password" id="password" onChange={handlePasswordChange} />
-
-      <button className='btn__login-form' type="submit">Login</button>
-    </form>
+    <section>
+      <h1>Acesso Administrador</h1>
+      <form action='' onSubmit={handleSubmit}>
+        <Input label='E-mail administrador' type='texto' name='username' {...username} />
+        <Input label='Senha' type='password' name='password' {...password} />
+        <Button>Entrar</Button>
+      </form>
+    </section>
   );
 };
 
